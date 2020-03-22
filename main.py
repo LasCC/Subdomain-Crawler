cc14bb1f836ec12b1ec57db146fbbf74b9a3f211
Ludovic COULON <43119034+LasCC@users.noreply.github.com>
2020-03-23 00:09:59 +0100

    Update the argparse implementation

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import requests
-import optparse
+import argparse
 
 def get_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--domain", dest="domain", help="Domain that you want to crawl")
     parser.add_argument("-w", "--worlist", dest="wordlist", help="Wordlist default is in the working directory", default="./wordlist.txt")
     options = parser.parse_args()
-    if not options.url:
+    if not options.domain:
         parser.error("[!] Please add an domain to proceed, --help for more informations.")
     if not options.wordlist:
         parser.error("[!] Please add a wordlist to proceed, --help for more informations.")
@@ -18,11 +18,12 @@ def req(url):
     except requests.exceptions.ConnectionError:
         pass
 
-target_url = "google.com"
-with open("./wordlist.txt", "r") as wordlist:
-    for l in wordlist:
+options = get_arguments()
+target_url = str(options.domain)
+with open(options.wordlist, "r") as wordlists:
+    for l in wordlists:
         w = l.strip()
         url = w + "." + target_url
         res = req(url)
         if res:
-            print("[+] Subdomain found ! > " + str(url))
+            print("[+] Subdomain found ! > " + url)
